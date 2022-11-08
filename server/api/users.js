c578035a8a39f14548bd3589c6c9473b59e579a2
Jane Yeh <jane.yeh72@gmail.com>
2022-11-08 16:20:37 -0500

    user routes

--- server/api/users.js
+++ server/api/users.js
@@ -1,6 +1,6 @@
 const usersRouter = require('express').Router();
 const {
-  models: { User },
+  models: { User, Order },
 } = require('../db');
 module.exports = usersRouter;
 
@@ -15,5 +15,45 @@ usersRouter.get('/', async (req, res, next) => {
     res.json(users);
   } catch (err) {
     next(err);
+  }
+});
+
+usersRouter.get('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId, {
+      include: Order,
+    });
+    res.send(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.post("/", async (req, res, next) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.send(newUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.delete('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId);
+    await userId.destroy();
+    res.send(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.put('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId);
+    const response = await user.update(req.body)
+    res.send(response);
+  } catch (error) {
+    next(error);
   }
 });
