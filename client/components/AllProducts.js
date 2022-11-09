4b16e8ae6cab5da93eb67c05568a37a026933c74
Sheyla De los Santos <delossantosshey@gmail.com>
2022-11-09 16:40:22 -0500

    A few adjustments I had missed before

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -5,25 +5,23 @@ class AllProducts extends Component {
   render() {
     const { products } = this.props;
     return (
-      <div>
-        <section>
-          <h2 id="products-title">ALL PRODUCTS</h2>
-          <div id="products-container">
-            {this.props.products.map((product) => {
-              return (
-                <div key={product.id} id="product">
-                  <Link to={`/products/${product.id}`}>
-                    {' '}
-                    <p>{product.name}</p>
-                  </Link>
-                  <img id="product-image" src={product.image} />
-                  <p>{product.price}</p>
-                </div>
-              );
-            })}
-          </div>
-        </section>
-      </div>
+      <section>
+        <h2 id="products-title">ALL PRODUCTS</h2>
+        <div id="products-container">
+          {products.map((product) => {
+            return (
+              <div key={product.id} id="product">
+                <Link to={`/products/${product.id}`}>
+                  {' '}
+                  <p>{product.name}</p>
+                </Link>
+                <img id="product-image" src={product.image} />
+                <p>{product.price}</p>
+              </div>
+            );
+          })}
+        </div>
+      </section>
     );
   }
 }
