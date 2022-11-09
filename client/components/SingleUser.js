23327840f26bb0070b47c7332a0d4f78c2281070
Jane Yeh <jane.yeh72@gmail.com>
2022-11-09 12:06:25 -0500

    single user view

---
+++ client/components/SingleUser.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchSingleUser } from '../redux/singleUser';
+import { Link } from 'react-router-dom';
+
+export class SingleUser extends React.Component {
+  constructor() {
+    super();
+    this.state = [];
+  }
+  componentDidMount() {
+    try {
+      this.props.fetchSingleUser(this.props.match.params.userId);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  render() {
+    const { first_name, last_name, email } = this.props.user;
+    return (
+      <div>
+        <h1>Welcome {first_name}</h1>
+        <h3>Personal Information</h3>
+        <ul>
+          <li>First Name: {first_name}</li>
+          <li>Last Name: {last_name}</li>
+          <li>email: {email}</li>
+          <hr />
+        </ul>
+        <h3>Past Orders</h3>
+      </div>
+    );
+  }
+}
+
+const mapState = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+const mapDispatch = (dispatch) => {
+  return {
+    fetchSingleUser: (id) => {
+      dispatch(fetchSingleUser(id));
+    },
+  };
+};
+export default connect(mapState, mapDispatch)(SingleUser);
