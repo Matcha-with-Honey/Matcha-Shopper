707d0aa0d23deb4fb19e6b1ee74e35a72ac6c26c
Kate Grant <hello.kategrant@gmail.com>
2022-11-08 21:53:53 -0500

    added through table model with quantity to many to many association

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,22 @@
-const db = require('./db');
-const User = require('./models/user');
-const Product = require('./models/product');
+const db = require("./db");
+const User = require("./models/user");
+const Product = require("./models/product");
+const Order = require("./models/order");
+const Order_Products = require("./models/orderProducts");
 
 //associations could go here!
+Order.belongsToMany(Product, { through: "order_products" });
+Product.belongsToMany(Order, { through: "order_products" });
+
+User.hasMany(Order);
+Order.belongsTo(User);
 
 module.exports = {
   db,
   models: {
     User,
     Product,
+    Order,
+    Order_Products,
   },
 };
