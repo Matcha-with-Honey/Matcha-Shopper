82ed87b0b111b7c5b409ddaf98876bd40263d7c7
Kate Grant <hello.kategrant@gmail.com>
2022-11-08 21:54:19 -0500

    added mock order and through table data to seed

--- seed/seed.js
+++ seed/seed.js
@@ -1,7 +1,8 @@
 const {
   db,
-  models: { User, Product },
+  models: { User, Product, Order },
 } = require("../server/db/index.js");
+const Order_Products = require("../server/db/models/orderProducts.js");
 const { userData, productData } = require("./seedData");
 
 const seed = async () => {
@@ -10,6 +11,22 @@ const seed = async () => {
 
     await User.bulkCreate(userData);
     await Product.bulkCreate(productData);
+
+    // mock insert of joined table order_products at time of order insert
+    const orderData = [
+      {
+        purchase_status: false,
+        shipping_address: "19 Healthy Way, New York, New York, 10001",
+        userId: 3,
+      },
+    ];
+    const orderProductData = [
+      { orderId: 1, productId: 1, quanity: 2 },
+      { orderId: 1, productId: 3, quanity: 1 },
+    ];
+    await Order.bulkCreate(orderData);
+    await Order_Products.bulkCreate(orderProductData);
+
     console.log("Successful Seed!");
   } catch (error) {
     console.error("Something went wrong during db seed");
