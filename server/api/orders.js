e59ad5a7a3a3df1a9f6c96e00509411be44283a2
Kate Grant <hello.kategrant@gmail.com>
2022-11-09 12:55:02 -0500

    added order express routes

---
+++ server/api/orders.js
@@ -0,0 +1,40 @@
+const ordersRouter = require("express").Router();
+const {
+  models: { Product, Order },
+} = require("../db");
+
+ordersRouter.get("/:id", async (req, res, next) => {
+  try {
+    const order = await Order.findByPk(req.params.id, {
+      include: { model: Product },
+      where: {
+        orderId: req.params.id,
+      },
+    });
+    res.status(200).send(order);
+  } catch (error) {
+    next(error);
+  }
+});
+
+ordersRouter.get("/", async (req, res, next) => {
+  try {
+    const orders = await Order.findAll({
+      include: { model: Product },
+    });
+    res.status(200).send(orders);
+  } catch (error) {
+    next(error);
+  }
+});
+
+ordersRouter.post("/", async (req, res, next) => {
+  try {
+    const order = await Order.create(req.body);
+    res.status(201).send(order);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = ordersRouter;
