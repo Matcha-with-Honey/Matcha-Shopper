9049b1ef536f96133e8ae4cd0383b40b6817a71d
Kate Grant <hello.kategrant@gmail.com>
2022-11-10 12:36:39 -0500

    added action type, action creator, thunk, and reducer case for SET_SINGLE_CART

--- client/redux/orders.js
+++ client/redux/orders.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
 
 const SET_NEW_CART = 'SET_NEW_CART';
+const SET_SINGLE_CART = 'GET_SINGLE_CART';
 
 export const setNewCart = (cart) => ({
   type: SET_NEW_CART,
+  cart,
+});
+
+export const setSingleCart = (cart) => ({
+  type: SET_SINGLE_CART,
   cart,
 });
 
@@ -18,10 +24,23 @@ export const fetchNewCart = () => {
   };
 };
 
+export const fetchSingleCart = () => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get('/api/orders/:id');
+      dispatch(setNewCart(data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+
 const initialState = { order: null, cartItems: [] };
 const orderReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_NEW_CART:
+      return { ...state, order: action.cart };
+    case SET_SINGLE_CART:
       return { ...state, order: action.cart };
   }
 };
