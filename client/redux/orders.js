efb648ab7bdcaeefe79dcbd5eb61b76f6d05ee48
Kate Grant <hello.kategrant@gmail.com>
2022-11-10 15:05:27 -0500

    added action type, action creator, thunk, and reducer case for ADD_ITEM

--- client/redux/orders.js
+++ client/redux/orders.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const SET_NEW_CART = 'SET_NEW_CART';
 const SET_SINGLE_CART = 'GET_SINGLE_CART';
+const ADD_ITEM = 'ADD_ITEM';
 
 export const setNewCart = (cart) => ({
   type: SET_NEW_CART,
@@ -11,6 +12,11 @@ export const setNewCart = (cart) => ({
 export const setSingleCart = (cart) => ({
   type: SET_SINGLE_CART,
   cart,
+});
+
+export const setItem = (item) => ({
+  type: ADD_ITEM,
+  item,
 });
 
 export const fetchNewCart = () => {
@@ -24,11 +30,24 @@ export const fetchNewCart = () => {
   };
 };
 
-export const fetchSingleCart = () => {
+export const fetchSingleCart = (id) => {
+  // note: order id -- get from state.order.id
   return async (dispatch) => {
     try {
-      const { data } = await axios.get('/api/orders/:id');
+      const { data } = await axios.get(`/api/orders/:${id}`);
       dispatch(setNewCart(data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+
+export const addItem = (item) => {
+  // note: item {orderId:, productId:, quantity:}
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.post('/api/cart', item);
+      dispatch(addItem(data));
     } catch (error) {
       console.error(error);
     }
@@ -42,6 +61,10 @@ const orderReducer = (state = initialState, action) => {
       return { ...state, order: action.cart };
     case SET_SINGLE_CART:
       return { ...state, order: action.cart };
+    case ADD_ITEM: {
+      const updatedItems = [...state.cartItems, action.item];
+      return { ...state, cartItems: updatedItems };
+    }
   }
 };
 
