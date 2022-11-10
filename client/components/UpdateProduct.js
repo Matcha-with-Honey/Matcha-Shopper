f255dc34bbefc9baeb21c94c054b6d016a3506dd
Sheyla De los Santos <delossantosshey@gmail.com>
2022-11-09 19:28:24 -0500

    Created form to update a product

---
+++ client/components/UpdateProduct.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class UpdateProduct extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      description: '',
+      quantity: null,
+      image: '',
+      price: null,
+      category: '',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+    this.setState({
+      name: '',
+      description: '',
+      quantity: null,
+      image: '',
+      price: null,
+      category: '',
+    });
+  }
+
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <section>
+        <form id="update-product-form" onSubmit={this.handleSubmit}>
+          <input
+            name="name"
+            placeholder="product name"
+            value={this.state.name}
+            onChange={this.handleChange}
+          ></input>
+          <input
+            name="description"
+            placeholder="description"
+            value={this.state.description}
+            onChange={this.handleChange}
+          ></input>
+          <input
+            name="quantity"
+            placeholder="quantity"
+            value={this.state.quantity}
+            onChange={this.handleChange}
+          ></input>
+          <input
+            name="image"
+            placeholder="image"
+            value={this.state.image}
+            onChange={this.handleChange}
+          ></input>
+          <input
+            name="price"
+            price
+            placeholder="price"
+            value={this.state.price}
+            onChange={this.handleChange}
+          ></input>
+          <input
+            name="category"
+            placeholder="category"
+            value={this.state.category}
+            onChange={this.handleChange}
+          ></input>
+          <input type={submit}>UPDATE</input>
+        </form>
+      </section>
+    );
+  }
+}
+
+export default connect()(UpdateProduct);
