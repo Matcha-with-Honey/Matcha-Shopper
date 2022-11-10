85bf36c05bbdb6d3f627065fde086c882491cb82
Kate Grant <hello.kategrant@gmail.com>
2022-11-10 00:26:30 -0500

    added sub router to main router

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 router.use('/users', require('./users'));
 router.use('/products', require('./products'));
+router.use('/orders', require('./orders'));
+router.use('/cart', require('./cart'));
 
 router.use(async (req, res, next) => {
   const error = new Error('Not Found');
