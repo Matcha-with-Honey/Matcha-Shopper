d0aa8309c1d0a5c5de080f5dd53892d22d90b765
Jane Yeh <jane.yeh72@gmail.com>
2022-11-11 18:19:21 -0500

    existing value autofill in edit form

--- client/components/UpdateUser.js
+++ client/components/UpdateUser.js
@@ -26,43 +26,57 @@ class UpdateUser extends React.Component {
     this.props.editUser({ ...this.props.singleUser, ...this.state });
   }
 
+  componentDidUpdate(prevProps) {
+    if (prevProps.singleUser.id !== this.props.singleUser.id) {
+      this.setState({
+        first_name: this.props.singleUser.first_name,
+        last_name: this.props.singleUser.last_name,
+        username: this.props.singleUser.username,
+        password: this.props.singleUser.password,
+        email: this.props.singleUser.email,
+      });
+    }
+  }
+
   render() {
+    console.log(this.props);
+    console.log(this.state);
+
     return (
       <section>
         <form onSubmit={this.handleSubmit}>
           <label>First Name</label>
           <input
             name="first_name"
-            placeholder="first_name"
-            value={this.state.name}
+            value={this.state.first_name}
             onChange={this.handleChange}
           ></input>
           <label>Last Name</label>
           <input
             name="last_name"
             placeholder="last_name"
-            value={this.state.name}
+            value={this.state.last_name}
             onChange={this.handleChange}
           ></input>
           <label>Username</label>
           <input
             name="username"
             placeholder="username"
-            value={this.state.name}
+            value={this.state.username}
             onChange={this.handleChange}
           ></input>
           <label>Password</label>
           <input
             name="password"
             placeholder="password"
-            value={this.state.name}
+            value={this.state.password}
             onChange={this.handleChange}
           ></input>
           <label>Email</label>
           <input
             name="email"
             placeholder="email"
-            value={this.state.name}
+            value={this.state.email}
             onChange={this.handleChange}
           ></input>
           <button type="submit">Update</button>
