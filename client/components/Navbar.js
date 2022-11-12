5edc5c65969ae74b15ee8bf7340f23243b17a752
Jane Yeh <jane.yeh72@gmail.com>
2022-11-12 17:42:02 -0500

    deleted merge conflict

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -3,34 +3,6 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logout } from '../redux/auth';
 
-
-const Navbar = ({ handleClick, isLoggedIn }) => (
-  <div>
-    <h1>Matcha 🍵</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <Link to="/products">Products</Link>
-          <Link to="/users">Users</Link>
-          <Link to="/cart">Cart</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
-);
-
 const Navbar = (props) => {
   const { handleClick, isLoggedIn } = props;
   return (
@@ -71,7 +43,6 @@ const Navbar = (props) => {
     </div>
   );
 };
-
 
 /**
  * CONTAINER
