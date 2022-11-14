4faa2cd8b65a6370404786be278441c5a26bd14a
Kate Grant <hello.kategrant@gmail.com>
2022-11-14 16:33:34 -0500

    refresh on logout

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -3,46 +3,58 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logout } from '../redux/auth';
 
-const Navbar = (props) => {
-  const { handleClick, isLoggedIn } = props;
-  return (
-    <div>
-      <h1>Matcha 🍵</h1>
-      <nav>
-        {isLoggedIn && props.role === 'admin' && (
-          <div>
-            {/* The navbar will show these links after you log in */}
-            <Link to="/">Home</Link>
-            <Link to="/products">Products</Link>
-            <Link to="/users">Users</Link>
-            <Link to="/cart">Cart</Link>
-            <a onClick={handleClick}>Logout</a>
-          </div>
-        )}{' '}
-        {isLoggedIn && props.role === 'member' && (
-          <div>
-            {/* The navbar will show these links after you log in */}
-            <Link to="/">Home</Link>
-            <Link to="/products">Products</Link>
-            <Link to="/account">Account</Link>
-            <Link to="/cart">Cart</Link>
-            <a onClick={handleClick}>Logout</a>
-          </div>
-        )}
-        {!isLoggedIn && (
-          <div>
-            {/* The navbar will show these links before you log in */}
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
-            <Link to="/products">Products</Link>
-            <Link to="/cart">Cart</Link>
-          </div>
-        )}
-      </nav>
-      <hr />
-    </div>
-  );
-};
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.logoutHandler = this.logoutHandler.bind(this);
+  }
+
+  logoutHandler() {
+    this.props.handleClick();
+    window.location.reload();
+  }
+
+  render() {
+    const { isLoggedIn } = this.props;
+    return (
+      <div>
+        <h1>Matcha 🍵</h1>
+        <nav>
+          {isLoggedIn && this.props.role === 'admin' && (
+            <div>
+              {/* The navbar will show these links after you log in */}
+              <Link to="/">Home</Link>
+              <Link to="/products">Products</Link>
+              <Link to="/users">Users</Link>
+              <Link to="/cart">Cart</Link>
+              <a onClick={() => this.logoutHandler()}>Logout</a>
+            </div>
+          )}{' '}
+          {isLoggedIn && this.props.role === 'member' && (
+            <div>
+              {/* The navbar will show these links after you log in */}
+              <Link to="/">Home</Link>
+              <Link to="/products">Products</Link>
+              <Link to="/account">Account</Link>
+              <Link to="/cart">Cart</Link>
+              <a onClick={() => this.logoutHandler()}>Logout</a>
+            </div>
+          )}
+          {!isLoggedIn && (
+            <div>
+              {/* The navbar will show these links before you log in */}
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Sign Up</Link>
+              <Link to="/products">Products</Link>
+              <Link to="/cart">Cart</Link>
+            </div>
+          )}
+        </nav>
+        <hr />
+      </div>
+    );
+  }
+}
 
 /**
  * CONTAINER
