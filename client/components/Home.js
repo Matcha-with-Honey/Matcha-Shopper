1f0236c64151c4db019f92120835806f1d01c57f
Sheyla De los Santos <delossantosshey@gmail.com>
2022-11-15 22:33:55 -0500

    Added home main image, description and shop now button

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-/**
- * COMPONENT
- */
 export const Home = (props) => {
-  // const { username } = props;
-  // return username ? (
-  //   <div>
-  //     <h3>Welcome, {username}</h3>
-  //   </div>
-  // ) : (
-  //   <div>
-  //     <h3>Welcome!</h3>
-  //   </div>
-  // );
+  return (
+    <div>
+      <section id="home-main">
+        <h1 id="store-name">ALL MATCHA</h1>
+        <img id="home-image" src="homePage.jpg"></img>
+        <p id="home-desc">Where you find all things matcha</p>
+        <Link to="/products">SHOP NOW</Link>
+      </section>
+    </div>
+  );
 };
 
-/**
- * CONTAINER
- */
 const mapState = (state) => {
   return {
     username: state.authReducer.first_name,
