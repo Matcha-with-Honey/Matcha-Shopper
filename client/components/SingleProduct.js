4bb5d3f4d878d399e5e9a93d09e95703e81ad9e9
Sheyla De los Santos <delossantosshey@gmail.com>
2023-01-19 19:14:14 -0500

    Updated the single product view for admin

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchSingleProduct } from '../redux/products';
+import { fetchSingleProduct, persistProductUpdate } from '../redux/products';
 import { addItem, fetchNewCart } from '../redux/orders';
 import { useParams } from 'react-router-dom';
-import UpdateProduct from './UpdateProduct';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function withParams(Component) {
   return (props) => <Component {...props} params={useParams()} />;
@@ -14,11 +15,52 @@ class SingleProduct extends Component {
     super(props);
     this.state = {
       quantityToAdd: 1,
+      name: '',
+      description: '',
+      quantity: '',
+      image: '',
+      price: '',
+      category: '',
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleAlert = this.handleAlert.bind(this);
     this.handleAddItem = this.handleAddItem.bind(this);
   }
+
   componentDidMount() {
     this.props.fetchSingleProduct(this.props.params.productId);
+  }
+
+  componentDidUpdate(prevProps) {
+    const { name, description, quantity, image, price, category } =
+      this.props.product;
+    if (prevProps.product.id !== this.props.product.id) {
+      this.setState({
+        name,
+        description,
+        quantity,
+        image,
+        price,
+        category,
+      });
+    }
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+    this.props.persistProductUpdate(this.props.product, this.state);
+  }
+
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value,
+    });
+  }
+
+  handleAlert(event) {
+    event.preventDefault();
+    return toast(`The information has been updated.`);
   }
 
   makeOpts(product) {
@@ -62,22 +104,69 @@ class SingleProduct extends Component {
   }
 
   render() {
-    const { product } = this.props;
     const { role } = this.props;
+    const { product } = this.props;
+    let { name, description, quantity, image, price, category } = this.state;
 
     return (
       <div>
         {role === 'admin' ? (
           <section id="single-product-container">
-            <img src={product.image} />
-            <div id="inner-single-product">
-              <h2 id="product-name">{product.name}</h2>
-              <p>Quantity: {product.quantity}</p>
-              <p>Price: {product.price}</p>
-              <p>Description: {product.description}</p>
-              <p>Category: {product.category}</p>
-            </div>
-            <UpdateProduct />
+            <img id="product-image-single" src={product.image} />
+            <form
+              id="inner-single-product-update"
+              onSubmit={(event) => {
+                this.handleSubmit(event);
+                this.handleAlert(event);
+              }}
+            >
+              <label>PRODUCT NAME</label>
+              <input
+                id="product-name-update"
+                name="name"
+                value={name}
+                onChange={this.handleChange}
+              ></input>
+              <label>DESCRIPTION</label>
+              <input
+                id="product-description-update"
+                name="description"
+                value={description}
+                onChange={this.handleChange}
+              ></input>
+              <label>QUANTITY</label>
+              <input
+                name="quantity"
+                value={quantity}
+                onChange={this.handleChange}
+              ></input>
+              <label>IMAGE URL</label>
+              <input
+                name="image url"
+                value={image}
+                onChange={this.handleChange}
+              ></input>
+              <label>PRICE</label>
+              <input
+                name="price"
+                value={price}
+                onChange={this.handleChange}
+              ></input>
+              <label>CATEGORY</label>
+              <input
+                name="category"
+                placeholder="category"
+                value={category}
+                onChange={this.handleChange}
+              ></input>
+              <button type="submit">UPDATE</button>
+            </form>
+            <ToastContainer
+              position="top-center"
+              autoClose={1700}
+              hideProgressBar={true}
+              closeOnClick="true"
+            />
           </section>
         ) : (
           <section id="single-product-container">
@@ -85,7 +174,7 @@ class SingleProduct extends Component {
             <div id="inner-single-product">
               <h2 id="product-name">{product.name}</h2>
               <p id="product-desc">{product.description}</p>
-              <p id="product-single-price">{product.price}</p>
+              <p id="product-single-price">${product.price}</p>
               <div id="button-and-form">
                 <form>
                   <select
@@ -130,6 +219,8 @@ const mapDispatch = (dispatch) => {
     addItem: (productId, orderId, qty) =>
       dispatch(addItem(productId, orderId, qty)),
     fetchNewCart: (id) => dispatch(fetchNewCart(id)),
+    persistProductUpdate: (productId, product) =>
+      dispatch(persistProductUpdate(productId, product)),
   };
 };
 
